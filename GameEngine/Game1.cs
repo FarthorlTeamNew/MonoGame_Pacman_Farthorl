3954375855da4345445a7085ff6d471512e791d2
IvayloKodov <Ivailo_Kodov@abv.bg>
2016-07-15 17:01:47 +0300

    Changed background color :D

--- GameEngine/Game1.cs
+++ GameEngine/Game1.cs
@@ -95,7 +95,7 @@ namespace GameEngine
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
+            GraphicsDevice.Clear(Color.SkyBlue);
             if (this.pacMan.Health != 0)
             {
                 this.spriteBatch.Begin();
