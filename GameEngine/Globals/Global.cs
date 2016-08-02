a15f23d4e503dd23f2019d15e1d8b1870e009a7c
IvayloKodov <Ivailo_Kodov@abv.bg>
2016-08-02 11:07:07 +0300

    nothing

--- GameEngine/Globals/Global.cs
+++ GameEngine/Globals/Global.cs
@@ -5,7 +5,7 @@
         public static int quad_Width = 32;
         public static int quad_Height = 32;
         public static int GLOBAL_WIDTH = 768;
-        public static int GLOBAL_HEIGHT = 416;
+        public static int GLOBAL_HEIGHT = 456;
         public static int XMax = 24;
         public static int YMax = 13;
         public static int PacmanSpeed = 4;
