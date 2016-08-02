5341a211bd42735dd7afe1d333e4bda0cba213f8
Petar Dimitrov <lbmikesmith@gmail.com>
2016-08-02 14:26:30 +0300

    Cherries throw 2 of the ghosts randomly on the map

--- GameEngine/Models/LevelObjects/Fruits/Cherry.cs
+++ GameEngine/Models/LevelObjects/Fruits/Cherry.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using GameEngine.Models.LevelObjects.Ghosts;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace GameEngine.Models.LevelObjects.Fruits
@@ -14,7 +16,18 @@ namespace GameEngine.Models.LevelObjects.Fruits
 
         public override void ActivatePowerup(GhostGenerator ghostGen, PacMan pacMan)
         {
-
+            if (ghostGen.Ghosts.ContainsKey(nameof(Blinky)))
+            {
+                ghostGen.Ghosts[nameof(Blinky)].X = new Random().Next(3, 5) * 32;
+                ghostGen.Ghosts[nameof(Blinky)].Y = new Random().Next(2, 4) * 32;
+                ghostGen.GhostMovements[nameof(Blinky)].Reset();
+            }
+            if (ghostGen.Ghosts.ContainsKey(nameof(Pinky)))
+            {
+                ghostGen.Ghosts[nameof(Pinky)].X = new Random().Next(17, 21) * 32;
+                ghostGen.Ghosts[nameof(Pinky)].Y = new Random().Next(8, 11) * 32;
+                ghostGen.GhostMovements[nameof(Pinky)].Reset();
+            }
         }
     }
 }
