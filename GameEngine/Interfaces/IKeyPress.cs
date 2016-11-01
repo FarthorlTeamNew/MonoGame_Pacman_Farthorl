37435c9598bec087574e291cfc8de46a16d4b087
Doncho Patronski <donstz@yahoo.com>
2016-11-01 13:39:46 +0200

    Added interface

---
+++ GameEngine/Interfaces/IKeyPress.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace GameEngine.Interfaces
+{
+    public interface IKeyPress
+    {
+        bool IsPressedKey(Keys key, KeyboardState oldState);
+
+        bool IsReleasedKey(Keys key, KeyboardState oldState);
+    }
+}
