8d7f22a53e07807da526cca2befd9bcdc8ef7ff2
Stoyan Ivanov <saivanovs@gmail.com>
2016-11-27 22:57:39 +0200

    -Commit missing files

---
+++ PacMan/Menu/PlaceHolderTextBox.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Pacman.Menu
+{
+    public class PlaceHolderTextBox : TextBox
+    {
+
+        bool isPlaceHolder = true;
+        private System.ComponentModel.IContainer components;
+        string _placeHolderText;
+        public string PlaceHolderText
+        {
+            get { return _placeHolderText; }
+            set
+            {
+                _placeHolderText = value;
+                setPlaceholder();
+            }
+        }
+
+        //when the control loses focus, the placeholder is shown
+        private void setPlaceholder()
+        {
+            if (string.IsNullOrEmpty(this.Text))
+            {
+                this.Text = PlaceHolderText;
+                this.ForeColor = Color.Gray;
+                this.Font = new Font(this.Font, FontStyle.Italic);
+                isPlaceHolder = true;
+            }
+        }
+
+        //when the control is focused, the placeholder is removed
+        private void removePlaceHolder()
+        {
+
+            if (isPlaceHolder)
+            {
+                this.Text = "";
+                this.ForeColor = System.Drawing.SystemColors.WindowText;
+                this.Font = new Font(this.Font, FontStyle.Regular);
+                isPlaceHolder = false;
+            }
+        }
+        public PlaceHolderTextBox()
+        {
+            GotFocus += removePlaceHolder;
+            LostFocus += setPlaceholder;
+        }
+
+        private void setPlaceholder(object sender, EventArgs e)
+        {
+            setPlaceholder();
+        }
+
+        private void removePlaceHolder(object sender, EventArgs e)
+        {
+            removePlaceHolder();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Pacman.Data/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Pacman.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Pacman.Data.PacmanContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Pacman.Data.PacmanContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
