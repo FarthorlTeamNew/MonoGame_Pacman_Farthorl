89796ad15ce844e202588117c41f5b52e7fb849a
Stoyan Ivanov <saivanovs@gmail.com>
2016-12-06 20:58:01 +0200

    -Commit LevelCoordinates class

---
+++ Pacman.Models/LevelCoordinate.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pacman.Models
+{
+    public class LevelCoordinate
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("LevelId")]
+        public virtual Level Level { get; set; }
+        public int LevelId { get; set; }
+
+        [Required]
+        public int QuadrantX { get; set; }
+        [Required]
+        public int QuadrantY { get; set; }
+        [Required]
+        public bool isWall { get; set; }
+        [Required]
+        public bool isPoint { get; set; }
+    }
+}
