907fa1e8e8f0bc245fc675c56e5993e3d89818a0
Stoyan Ivanov <saivanovs@gmail.com>
2016-12-07 22:24:02 +0200

    -Update LevelCoordinate model to not able to change the properties. -This is very important becouse if the code change it, the levels will brokes.

--- Pacman.Models/LevelCoordinate.cs
+++ Pacman.Models/LevelCoordinate.cs
@@ -6,19 +6,19 @@ namespace Pacman.Models
     public class LevelCoordinate
     {
         [Key]
-        public int Id { get; set; }
+        public int Id { get; protected set; }
 
         [ForeignKey("LevelId")]
-        public virtual Level Level { get; set; }
-        public int LevelId { get; set; }
+        public virtual Level Level { get; protected set; }
+        public int LevelId { get; protected set; }
 
         [Required]
-        public int QuadrantX { get; set; }
+        public int QuadrantX { get; protected set; }
         [Required]
-        public int QuadrantY { get; set; }
+        public int QuadrantY { get; protected set; }
         [Required]
-        public bool isWall { get; set; }
+        public bool isWall { get; protected set; }
         [Required]
-        public bool isPoint { get; set; }
+        public bool isPoint { get; protected set; }
     }
 }
