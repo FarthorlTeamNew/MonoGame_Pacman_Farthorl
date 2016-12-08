a87e108d91a83d12aa028381fe1999e2ef05b0c2
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-08 17:40:36 +0200

    Added another Global class. Static for now for easy access, might change it later to encapsulate it.

---
+++ PacMan/Globals/GameStatistic.cs
@@ -0,0 +1,39 @@
+﻿namespace Pacman.Globals
+{
+    public static class GameStatistic
+    {
+        //Player stats
+
+        public static int PlayerPointsEaten { get; set; }
+
+        public static int PlayerFruitEatenCount { get; set; }
+
+        public static int PlayerGhostsEatenCount { get; set; }
+
+        public static int LevelsCompleted { get; set; }
+
+        public static int PlayerTimesDied { get; set; }
+
+        //Ghosts stats
+
+        public static int NumberOfPointsEatenByGhost { get; set; }
+
+        public static int TimesEatenPacman { get; set; }
+
+        public static void NullifyStats()
+        {
+            PlayerPointsEaten = 0;
+            PlayerFruitEatenCount = 0;
+            PlayerGhostsEatenCount = 0;
+            LevelsCompleted = 0;
+            PlayerTimesDied = 0;
+            NumberOfPointsEatenByGhost = 0;
+            TimesEatenPacman = 0;
+        }
+
+        public static void UpdateStatsToDatabase()
+        {
+            // Use DataBridge to update the stats of the current player
+        }
+    }
+}
