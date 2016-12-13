52a0e6caa237050f4a22881b1cc3ca628abd4fb1
Patronski <donstz@yahoo.com>
2016-12-13 03:16:19 +0200

    All necessary info is added to STATISTIC.

--- PacMan/Forms/Statistic.Designer.cs
+++ PacMan/Forms/Statistic.Designer.cs
@@ -38,6 +38,10 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.buttonUsers = new System.Windows.Forms.Button();
             this.buttonAnecdotes = new System.Windows.Forms.Button();
+            this.buttonPlayersStatistic = new System.Windows.Forms.Button();
+            this.buttonGameStatistic = new System.Windows.Forms.Button();
+            this.buttonUsersFriends = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.contextMenuStrip2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -94,9 +98,10 @@
             // 
             // buttonUsers
             // 
+            this.buttonUsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonUsers.Location = new System.Drawing.Point(12, 12);
             this.buttonUsers.Name = "buttonUsers";
-            this.buttonUsers.Size = new System.Drawing.Size(43, 37);
+            this.buttonUsers.Size = new System.Drawing.Size(52, 45);
             this.buttonUsers.TabIndex = 4;
             this.buttonUsers.Text = "Users";
             this.buttonUsers.UseVisualStyleBackColor = true;
@@ -104,19 +109,67 @@
             // 
             // buttonAnecdotes
             // 
-            this.buttonAnecdotes.Location = new System.Drawing.Point(61, 12);
+            this.buttonAnecdotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAnecdotes.Location = new System.Drawing.Point(331, 12);
             this.buttonAnecdotes.Name = "buttonAnecdotes";
-            this.buttonAnecdotes.Size = new System.Drawing.Size(67, 37);
+            this.buttonAnecdotes.Size = new System.Drawing.Size(81, 45);
             this.buttonAnecdotes.TabIndex = 5;
             this.buttonAnecdotes.Text = "Anecdotes";
             this.buttonAnecdotes.UseVisualStyleBackColor = true;
             this.buttonAnecdotes.Click += new System.EventHandler(this.buttonAnecdotes_Click);
+            // 
+            // buttonPlayersStatistic
+            // 
+            this.buttonPlayersStatistic.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlayersStatistic.Location = new System.Drawing.Point(157, 12);
+            this.buttonPlayersStatistic.Name = "buttonPlayersStatistic";
+            this.buttonPlayersStatistic.Size = new System.Drawing.Size(81, 45);
+            this.buttonPlayersStatistic.TabIndex = 6;
+            this.buttonPlayersStatistic.Text = "Players Statistic";
+            this.buttonPlayersStatistic.UseVisualStyleBackColor = true;
+            this.buttonPlayersStatistic.Click += new System.EventHandler(this.buttonPlayersStatistic_Click);
+            // 
+            // buttonGameStatistic
+            // 
+            this.buttonGameStatistic.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGameStatistic.Location = new System.Drawing.Point(244, 12);
+            this.buttonGameStatistic.Name = "buttonGameStatistic";
+            this.buttonGameStatistic.Size = new System.Drawing.Size(81, 45);
+            this.buttonGameStatistic.TabIndex = 7;
+            this.buttonGameStatistic.Text = "Game Statistic";
+            this.buttonGameStatistic.UseVisualStyleBackColor = true;
+            this.buttonGameStatistic.Click += new System.EventHandler(this.buttonGameStatistic_Click);
+            // 
+            // buttonUsersFriends
+            // 
+            this.buttonUsersFriends.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonUsersFriends.Location = new System.Drawing.Point(70, 12);
+            this.buttonUsersFriends.Name = "buttonUsersFriends";
+            this.buttonUsersFriends.Size = new System.Drawing.Size(81, 45);
+            this.buttonUsersFriends.TabIndex = 8;
+            this.buttonUsersFriends.Text = "Users - Friends";
+            this.buttonUsersFriends.UseVisualStyleBackColor = true;
+            this.buttonUsersFriends.Click += new System.EventHandler(this.buttonUsersFriends_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(434, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "This is a Game, Not a Newspaper!";
             // 
             // Statistic
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(690, 375);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonUsersFriends);
+            this.Controls.Add(this.buttonGameStatistic);
+            this.Controls.Add(this.buttonPlayersStatistic);
             this.Controls.Add(this.buttonAnecdotes);
             this.Controls.Add(this.buttonUsers);
             this.Controls.Add(this.dataGridView1);
@@ -125,6 +178,7 @@
             this.contextMenuStrip2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -138,5 +192,9 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button buttonUsers;
         private System.Windows.Forms.Button buttonAnecdotes;
+        private System.Windows.Forms.Button buttonPlayersStatistic;
+        private System.Windows.Forms.Button buttonGameStatistic;
+        private System.Windows.Forms.Button buttonUsersFriends;
+        private System.Windows.Forms.Label label1;
     }
 }
--- PacMan/Forms/Statistic.cs
+++ PacMan/Forms/Statistic.cs
@@ -25,7 +25,13 @@ namespace Pacman.Forms
 
         private void buttonUsers_Click(object sender, EventArgs e)
         {
-            SqlCommand command = new SqlCommand("select * from Users", connectionDataBase);
+            string sql = "SELECT u.Id, FirstName, LastName, BurthDate, DATEDIFF(year, [BurthDate], GETDATE()) as Age, c.Name as Country, ci.Name as City " +
+                                "FROM Users u " +
+                                "join Countries c " +
+                                "on u.CountryId = c.Id " +
+                                "join Cities ci " +
+                                "on u.CityId = ci.Id";
+            SqlCommand command = new SqlCommand(sql, connectionDataBase);
 
             ExecuteCommandInDataGrid(command);
         }
@@ -33,6 +39,46 @@ namespace Pacman.Forms
         private void buttonAnecdotes_Click(object sender, EventArgs e)
         {
             SqlCommand command = new SqlCommand("select * from Anecdotes", connectionDataBase);
+
+            ExecuteCommandInDataGrid(command);
+        }
+
+        private void buttonPlayersStatistic_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT (u.FirstName + ' ' + u.LastName) as UserName, PlayerPointsEaten, PlayerFruitEatenCount, PlayerGhostsEatenCount, PlayerGhostkillersEaten, HardLevelsCompleted, EasyLevelsCompleted, PlayerTimesDied " +
+                                 "FROM PlayerStatistics ps " +
+                                 "join Users u " +
+                                 "on ps.UserId = u.Id " +
+                                 "order by PlayerPointsEaten desc ";
+            SqlCommand command = new SqlCommand(sql, connectionDataBase);
+
+            ExecuteCommandInDataGrid(command);
+        }
+
+        private void buttonGameStatistic_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT s.Id ,(u.FirstName + ' ' + u.LastName) as UserName ,l.Name ,StartGame ,EndGame ,Duration " +
+                     "FROM [Statistics] s " +
+                     "join Users u " +
+                     "on s.UserId = u.Id " +
+                     "join Levels l " +
+                     "on s.LevelId = l.Id " +
+                     "order by s.StartGame desc ";
+            SqlCommand command = new SqlCommand(sql, connectionDataBase);
+
+            ExecuteCommandInDataGrid(command);
+        }
+
+        private void buttonUsersFriends_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT (u.FirstName + ' ' + u.LastName) as UserName, (f.FirstName + ' ' + f.LastName) as FriendName " +
+                             "FROM [dbo].[UsersFriends] uf " +
+                             "join Users u " +
+                             "on u.Id = uf.UserId " +
+                             "join Users f " +
+                             "on f.Id = uf.FriendId " +
+                             "order by uf.userId ";
+            SqlCommand command = new SqlCommand(sql, connectionDataBase);
 
             ExecuteCommandInDataGrid(command);
         }
